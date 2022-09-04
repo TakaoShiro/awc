96ffe18f9e158c6787ea81705ff8ccd2607e7698
EC2 Default User <ec2-user@ip-172-31-26-114.ap-northeast-1.compute.internal>
2022-09-04 10:42:30 +0000

    [Add]root修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
   devise_for :customers
   
   namespace :admin do
-    root to: 'homes/top'
+    root to: "homes#top"
     resources :sessions, only: [:new,:create,:destroy]
   end
   
   namespace :public do
-    root to: 'homes/top'
+    root to: "homes#top"
     resources :rooms, only: [:show,:create]
     resources :customers, only: [:index,:show,:edit,:update]
     get "customers/unsubscribe"
@@ -16,17 +16,7 @@ Rails.application.routes.draw do
     resources :sessions, only: [:new,:create,:destroy]
     resources :registrations, only: [:new,:create]
     resources :animals, only: [:index,:show]
-    get 'homes/about'
+    get "homes/about"
   end
-  
-end
-  
-    
-   
-    
-    
-    
-  
-  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

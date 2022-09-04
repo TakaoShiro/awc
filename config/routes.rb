ebcc13635f7e879f0070df456e049a182276d840
EC2 Default User <ec2-user@ip-172-31-26-114.ap-northeast-1.compute.internal>
2022-09-04 10:07:48 +0000

    [Add]root記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,32 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :admin do
-    get 'sessions/new'
-  end
-  namespace :public do
-    get 'rooms/show'
-  end
-  namespace :public do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-  end
-  namespace :public do
-    get 'sessions/new'
-    get 'sessions/create'
-    get 'sessions/destroy'
-  end
-  namespace :public do
-    get 'registrations/index'
-    get 'registrations/show'
-  end
-  namespace :public do
-    get 'animals/index'
-    get 'animals/show'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
   devise_for :admins
   devise_for :customers
+  
+  namespace :admin do
+    root to: 'homes/top'
+    resources :sessions, only: [:new,:create,:destroy]
+  end
+  
+  namespace :public do
+    root to: 'homes/top'
+    resources :rooms, only: [:show,:create]
+    resources :customers, only: [:index,:show,:edit,:update]
+    get "customers/unsubscribe"
+    get "customers/withdraw"
+    resources :sessions, only: [:new,:create,:destroy]
+    resources :registrations, only: [:new,:create]
+    resources :animals, only: [:index,:show]
+    get 'homes/about'
+  end
+  
+end
+  
+    
+   
+    
+    
+    
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

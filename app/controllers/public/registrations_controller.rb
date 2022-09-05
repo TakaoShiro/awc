3bf66545b49058fe7abcabdc922033d02be9676c
EC2 Default User <ec2-user@ip-172-31-26-114.ap-northeast-1.compute.internal>
2022-09-05 11:29:14 +0000

    [Add]新規会員登録の実装

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -4,4 +4,19 @@ class Public::RegistrationsController < ApplicationController
 
   def create
   end
+  
+  def after_sign_in_path_for(resource)
+    customer_path(current_customer)
+  end
+
+  def after_sign_out_path_for(resource)
+    "/"
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:name, :kana, :telephone_namber, :prefecture])
+    devise_parameter_sanitizer.permit(:sign_in, keys:[:email])
+  end
 end

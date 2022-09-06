b7a87fb189ebef81776a4ae5dc50a4cd7677372d
EC2 Default User <goemon.takao.75@gmail.com>
2022-09-06 07:05:15 +0000

    [Add]会員登録修正

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,6 +1,4 @@
 class Admin::HomesController < ApplicationController
   def top
-    #新着順
-    @animals = Animals.order('id DESC').limit(4)
   end
 end
--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
+    #新着順
+    @animals = Animal.order('id DESC').limit(4)
   end
 
   def about

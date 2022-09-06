be2b0a5d0fcf0ccee9a3ddce1c73db1a855dc132
EC2 Default User <goemon.takao.75@gmail.com>
2022-09-06 13:05:24 +0000

    [Add]新規登録エラー修正

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,7 +1,6 @@
 class Public::SessionsController < Devise::SessionsController
   
-  def new
-  end
+  before_action :customer_state, only: [:create]
   
   protected
   # 退会しているかを判断するメソッド
@@ -15,4 +14,32 @@ class Public::SessionsController < Devise::SessionsController
     redirect_to new_customer_registration_path
   end
   end
+  
+  #protected
+
+  #def configure_permitted_parameters
+    #devise_parameter_sanitizer.permit(:sign_up, keys:[:first_name, :last_name, :first_name_kana, :last_name_kana, :email, :address, :postal_code, :telephone_namber])
+    #devise_parameter_sanitizer.permit(:sign_in, keys:[:email])
+  #end
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
 end

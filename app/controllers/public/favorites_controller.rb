387552ae21392e280ea0addec4dedba88fd1350f
EC2 Default User <goemon.takao.75@gmail.com>
2022-09-11 07:13:54 +0000

    [Add]退会機能の実装

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -3,12 +3,12 @@ class Public::FavoritesController < ApplicationController
   def create
     @amimal_favorite = Favorite.new(customer_id: current_customer.id, animal_id: params[:animal_id])
     @amimal_favorite.save
-    redirect_to animals_path(params[:animal_id]) 
+    #非同期通信の為redirect無し
   end
   
   def destroy
     @animal_favorite = Favorite.find_by(customer_id: current_customer.id, animal_id: params[:animal_id])
     @animal_favorite.destroy
-    redirect_to animal_path(params[:animal_id]) 
+    #非同期通信の為redirect無し
   end
 end

68bb3dfedf948aa1cc1878acdf27d1e8a89ad326
EC2 Default User <goemon.takao.75@gmail.com>
2022-09-13 04:35:43 +0000

    管理者ログイン機能作成

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,5 +1,17 @@
-class Admin::SessionsController < ApplicationController
+# frozen_string_literal: true
+
+class Admin::SessionsController < Devise::SessionsController
    before_action :authenticate_admin!
+   
+protected
+  def after_sign_in_path_for(resource)
+    admin_root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+   
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
